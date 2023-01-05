2e0dc621ca60942ca7c7a8236dd25c329f559362
Catherine Miguel <catmig1992@gmail.com>
2023-01-05 00:48:14 -0800

    second test

--- src/App.test.js
+++ src/App.test.js
@@ -5,11 +5,24 @@ beforeEach(() => {
   fetch.resetMocks();
 });
 
-test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
-  fetch.mockResponseOnce(JSON.stringify({ name: "coder" }));
-  render(<App />);
-  const gitHubName = await waitFor(() =>
-    screen.getByRole("heading", { level: 2 })
-  );
-  expect(gitHubName).toHaveTextContent("coder");
+describe("receives values from GitHub REST API using jest fetch mock", () => {
+  test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ name: "coder" }));
+    render(<App />);
+    const gitHubName = await waitFor(() =>
+      screen.getByRole("heading", { level: 2 })
+    );
+    expect(gitHubName).toHaveTextContent("coder");
+  });
+  test("receives GitHub URL", async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify({ html_url: "https://github.com/learningToCode1234" })
+    );
+    render(<App />);
+    const gitHubURL = await waitFor(() => screen.getByRole("link"));
+    expect(gitHubURL).toHaveAttribute(
+      "href",
+      expect.stringContaining("github.com")
+    );
+  });
 });
