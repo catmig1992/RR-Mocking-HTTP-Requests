a31e9050a2e870ef0703a4edf466462c1af1f059
Catherine Miguel <catmig1992@gmail.com>
2023-01-05 00:19:35 -0800

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,15 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
 
+beforeEach(() => {
+  fetch.resetMocks();
+});
 
-
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+  fetch.mockResponseOnce(JSON.stringify({ name: "coder" }));
+  render(<App />);
+  const gitHubName = await waitFor(() =>
+    screen.getByRole("heading", { level: 2 })
+  );
+  expect(gitHubName).toHaveTextContent("coder");
+});
